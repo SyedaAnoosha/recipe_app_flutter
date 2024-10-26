434d34c112e38de5dce373f0ff18ee4a425aaa3e
Anoosha <anushazaidi06@gmail.com>
2024-10-26 15:29:33 +0500

    edited

--- lib/utils/favorite_icon.dart
+++ lib/utils/favorite_icon.dart
@@ -1,42 +1,8 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/material.dart';
-// import 'package:recipe_app_flutter/utils/constants.dart';
-// import 'package:recipe_app_flutter/provider/favorite_provider.dart';
-
-// class FavoriteIcon extends StatelessWidget {
-//   final DocumentSnapshot<Object?> documentSnapshot;
-
-//   const FavoriteIcon({super.key, required this.documentSnapshot});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final provider = FavoriteProvider.of(context);
-
-//     return CircleAvatar(
-//       backgroundColor: Colors.white70,
-//       child: GestureDetector(
-//         onTap: () {
-//           provider.toggleFavorite(documentSnapshot);
-//         },
-//         child: Icon(
-//           provider.isExist(documentSnapshot)
-//               ? Icons.favorite
-//               : Icons.favorite_border_outlined,
-//           color:
-//               provider.isExist(documentSnapshot) ? buttonColor : secondaryColor,
-//           size: 20,
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:recipe_app_flutter/provider/favorite_provider.dart';
 import 'package:recipe_app_flutter/utils/constants.dart';
-// import 'package:recipe_app_flutter/provider/favorite_provider.dart';
 
 class FavoriteIcon extends StatelessWidget {
   final DocumentSnapshot recipe;
